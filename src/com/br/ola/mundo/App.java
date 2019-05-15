f6d4bf15c415a755ad8aec790e89643f4e8475e6
Diego2207 <diego.santos@zup.com.br>
2019-05-15 17:36:05 -0300

    Criar programa do estoque

---
+++ src/com/br/ola/mundo/App.java
@@ -0,0 +1,26 @@
+package com.br.ola.mundo;
+import java.util.Scanner;
+
+public class App {
+	
+	public static void main(String[] args) {
+		System.out.println("ola mundo");
+			Scanner teclado = new Scanner(System.in);
+			
+			int capacidadeTotal = teclado.nextInt();
+			int minima = 20;
+			
+			System.out.println("A média é: " + (capacidadeTotal + minima) / 2);
+			
+			float capacidadeTotal2 = 1000f;
+			double minima2 = 55.7; 
+			
+					System.out.println(" a media é: " + (capacidadeTotal2 + minima2) / 2);
+					
+	
+			
+			
+			
+			
+	}
+}
